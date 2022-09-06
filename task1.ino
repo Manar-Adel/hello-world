849b8eab85f3c8538448322ef80af2dc6de23b80
Manar-Adel <manarkhedr02@gmail.com>
2022-09-06 23:36:41 +0200

    Second Commit

---
+++ task1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9,OUTPUT)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(9,1);
+  delay(500);
+  digitalWrite(9,0);
+
+}
